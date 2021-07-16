a41884e5856f2549ebfecf8add31eae594a3970c
雪千渔 <JomiXedYu@outlook.com>
2021-07-17 01:26:59 +0800

    arr

--- JxUnity.Common/Jxugui/Attributes/JuiPanelBaseAttribute.cs
+++ JxUnity.Common/Jxugui/Attributes/JuiPanelBaseAttribute.cs
@@ -12,10 +12,6 @@ public abstract class JuiPanelBaseAttribute : JuiAbstractAttribute
     /// </summary>
     public bool EnableUpdate { get; set; } = false;
     /// <summary>
-    /// 在场景初始化时将脚本绑定到UI
-    /// </summary>
-    public bool IsPreBind { get; set; } = false;
-    /// <summary>
     /// 资源加载路径，当UI不存在于场景时，进行动态加载
     /// </summary>
     public string ResourcePath { get; set; } = default;
@@ -23,6 +19,7 @@ public abstract class JuiPanelBaseAttribute : JuiAbstractAttribute
     /// 自动绑定元素，对类型内标示的字段进行自动绑定
     /// </summary>
     public bool IsAutoBindElement { get; set; } = true;
+
     public JuiPanelBaseAttribute() { }
     public JuiPanelBaseAttribute(string name)
     {
