049c8c8d108ad5268ba9aefbb5705b938d1fbeff
雪千渔 <JomiXedYu@outlook.com>
2021-07-28 02:37:48 +0800

    arrange

--- JxUnity/Utility/GameStateManager.cs
+++ JxUnity/Utility/GameStateManager.cs
@@ -1,30 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameRunningState
 {
     public enum RunningState
     {
         Running,
-        SpeedUp,
         Pause
     }
+    public enum SpeedState
+    {
+        Normal,
+        Speed,
+    }
 
-    public static RunningState State { get; private set; }
+    public static RunningState Running { get; private set; }
+    public static SpeedState Speed { get; private set; }
 
+    public static void Run()
+    {
+        Time.timeScale = 1;
+        Running = RunningState.Running;
+    }
     public static void Pause()
     {
         Time.timeScale = 0;
+        Running = RunningState.Pause;
+    }
+
+    public static void NormalSpeed()
+    {
+        Time.timeScale = 1;
+        Speed = SpeedState.Normal;
     }
     public static void SpeedUp(float speed)
     {
         Time.timeScale = speed;
+        Speed = SpeedState.Speed;
     }
-    public static void Run()
-    {
-        Time.timeScale = 1;
-    }
+
 
 }
