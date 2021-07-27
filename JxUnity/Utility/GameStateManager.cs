2d6fb4c3dfb29cf46b4155add3b4dc404cc162ec
雪千渔 <JomiXedYu@outlook.com>
2021-07-28 02:30:49 +0800

    Create GameStateManager.cs

---
+++ JxUnity/Utility/GameStateManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class GameRunningState
+{
+    public enum RunningState
+    {
+        Running,
+        SpeedUp,
+        Pause
+    }
+
+    public static RunningState State { get; private set; }
+
+    public static void Pause()
+    {
+        Time.timeScale = 0;
+    }
+    public static void SpeedUp(float speed)
+    {
+        Time.timeScale = speed;
+    }
+    public static void Run()
+    {
+        Time.timeScale = 1;
+    }
+
+}
