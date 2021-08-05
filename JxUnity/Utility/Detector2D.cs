fda9bca22027c1d961e25ed5378627257b02163b
雪千渔 <JomiXedYu@outlook.com>
2021-08-05 20:54:39 +0800

    Create Detector2D.cs

---
+++ JxUnity/Utility/Detector2D.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public static class Detector2D
+{
+
+    public static bool Sector(Vector2 point, Vector2 target, Vector2 dir, float radius, float degree)
+    {
+        Vector2 apos = point;
+        Vector2 bpos = target;
+        if (Vector2.Distance(apos, bpos) > radius)
+        {
+            return false;
+        }
+        if (Vector2.Angle(dir, bpos - apos) < degree / 2)
+        {
+            return true;
+        }
+        return false;
+    }
+    
+}
