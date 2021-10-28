a8259fe0e165a8b31500f6983f2bfdc9095f3428
JomiXedYu <JomiXedYu@outlook.com>
2021-10-28 17:32:16 +0800

    ugui

---
+++ JxUnity/UguiExtension/SequenceImage.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace JxUnity.Ugui
+{
+    [RequireComponent(typeof(Image))]
+    public class SequenceImage : MonoBehaviour
+    {
+        private Image image = null;
+
+        [SerializeField]
+        private SequenceImageAsset images;
+        public SequenceImageAsset ImageAsset { get => images; set => images = value; }
+
+        public float FrameRate { get => images.FrameRate; }
+        public float Speed { get => images.Speed; }
+
+        [SerializeField]
+        private bool isPlaying = false;
+        public bool IsPlaying { get => isPlaying; }
+
+        [SerializeField]
+        private int index = 0;
+        public int Index { get => index; }
+
+        [SerializeField]
+        private bool playOnAwake = false;
+        public bool PlayOnAwake { get => PlayOnAwake; }
+
+        private void Awake()
+        {
+            this.image = this.GetComponent<Image>();
+            if (this.image == null)
+            {
+                this.image = this.gameObject.AddComponent<Image>();
+            }
+            if (this.playOnAwake) this.Play();
+        }
+
+        public void Play()
+        {
+            this.isPlaying = true;
+            var rect = this.GetComponent<RectTransform>();
+            var targetRect = this.images[0].rect;
+            rect.sizeDelta = new Vector2(targetRect.width, targetRect.height);
+        }
+        public void Stop()
+        {
+            this.isPlaying = false;
+        }
+
+        private float count = 0;
+        private void Update()
+        {
+            if (this.isPlaying)
+            {
+                if (this.images == null || this.images.Count() == 0)
+                {
+                    return;
+                }
+                this.count += Time.deltaTime;
+                float frameTime = this.Speed / this.FrameRate;
+                if (this.count >= frameTime)
+                {
+                    this.index++;
+                    if (this.images.Count() == this.index) this.index = 0;
+                    this.image.sprite = this.images[this.index];
+                    this.count = 0;
+                }
+            }
+        }
+
+    }
+
+}
+
---
+++ JxUnity/UguiExtension/SequenceImageAsset.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JxUnity.Ugui
+{
+    [CreateAssetMenu(menuName = "JxUgui/Sequence Image Asset")]
+    public class SequenceImageAsset : ScriptableObject
+    {
+        [SerializeField]
+        public Sprite[] Images;
+        [SerializeField]
+        public float Speed = 1;
+        [SerializeField]
+        public float FrameRate = 15;
+
+        public int Count()
+        {
+            return this.Images.Length;
+        }
+
+        public Sprite this[int i]
+        {
+            get
+            {
+                return this.Images[i];
+            }
+        }
+    }
+}
