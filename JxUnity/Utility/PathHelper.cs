c193a56b4cf0987ffa62f5e717f45caa3762ef66
JomiXedYu <JomiXedYu@outlook.com>
2021-10-28 17:43:55 +0800

    Create PathHelper.cs

---
+++ JxUnity/Utility/PathHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using UnityEngine;
+using System;
+
+public static class PathHelper
+{
+    /// <summary>
+    /// 可写路径
+    /// </summary>
+    public static readonly string WritablePath = GetWritablePath();
+
+    public static readonly string Config = $"{WritablePath}/Config";
+    public static readonly string SaveData = $"{WritablePath}/SaveData";
+    public static readonly string Log = $"{WritablePath}/Log";
+    public static readonly string ScreenShot = $"{WritablePath}/ScreenShot";
+    public static readonly string Icon = $"{WritablePath}/Icon";
+
+
+    private static string GetWritablePath()
+    {
+        if (Application.isEditor)
+        {
+            return System.Environment.CurrentDirectory;
+        }
+        switch (Application.platform)
+        {
+            case RuntimePlatform.WindowsPlayer:
+                return System.Environment.CurrentDirectory;
+            case RuntimePlatform.Android:
+                return Application.persistentDataPath;
+            default:
+                break;
+        }
+        return null;
+    }
+
+}
