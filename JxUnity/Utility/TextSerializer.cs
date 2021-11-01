6d8ee847ddc003552b01812660954e6c33ae50da
JomiXedYu <JomiXedYu@outlook.com>
2021-11-01 11:14:06 +0800

    Update TextSerializer.cs

--- JxUnity/Utility/TextSerializer.cs
+++ JxUnity/Utility/TextSerializer.cs
@@ -1,63 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
-public class TextSerializer
+public abstract class TextSerializer
 {
-    public interface ITextSerializer
-    {
-        string Format { get; }
-        string Serialize(object obj);
-        object Deserialize(string text, Type type);
-    }
-    public class TextXmlSerializer : ITextSerializer
-    {
-        public class UTF8StringWriter : StringWriter
-        {
-            private static readonly Encoding UTF8 = new UTF8Encoding(false);
-            public override Encoding Encoding => UTF8;
-            public UTF8StringWriter(StringBuilder sb) : base(sb) { }
-        }
+    /// <summary>
+    /// System.Xml
+    /// </summary>
+    public static TextSerializer Xml { get; } = new TextXmlSerializer();
+    /// <summary>
+    /// Newtonsoft.Json
+    /// </summary>
+    public static TextSerializer Json { get; } = new TextNewtonJsonSerializer();
 
-        public string Format => "xml";
+    public abstract string Format { get; }
+    public abstract string Serialize(object obj);
+    public abstract object Deserialize(string text, Type type);
 
-        public string Serialize(object obj)
-        {
-            XmlSerializer xs = new XmlSerializer(obj.GetType());
-            StringBuilder sb = new StringBuilder();
-            UTF8StringWriter sw = new UTF8StringWriter(sb);
-            xs.Serialize(sw, obj);
-            sw.Close();
-            return sb.ToString();
-        }
-        public object Deserialize(string text, Type type)
-        {
-            XmlSerializer xs = new XmlSerializer(type);
-            StringReader sr = new StringReader(text);
-            return xs.Deserialize(sr);
-        }
-    }
-
-    public static ITextSerializer Serializer { get; set; } = new TextXmlSerializer();
-
-    public static string Serialize(object obj)
-    {
-        return Serializer.Serialize(obj);
-    }
-    public static object Deserialize(string text, Type type)
-    {
-        return Serializer.Deserialize(text, type);
-    }
-
-    public static T Deserialize<T>(string text)
-    {
-        return (T)Deserialize(text, typeof(T));
-    }
-
-    public static object Read(string path, Type type)
+    public object Read(string path, Type type)
     {
         if (!File.Exists(path))
         {
@@ -65,11 +28,12 @@ public class TextSerializer
         }
         return Deserialize(File.ReadAllText(path), type);
     }
-    public static T Read<T>(string path)
+    public T Read<T>(string path)
     {
         return (T)Read(path, typeof(T));
     }
-    public static void Write(string path, object obj)
+
+    public void Write(string path, object obj)
     {
         if (obj == null)
         {
@@ -83,7 +47,7 @@ public class TextSerializer
         File.WriteAllText(path, Serialize(obj));
     }
 
-    public static Task<Exception> WriteAsync(string path, object obj)
+    public Task<Exception> WriteAsync(string path, object obj)
     {
         return Task.Run<Exception>(() =>
         {
@@ -99,9 +63,10 @@ public class TextSerializer
             return e;
         });
     }
-    public static void WriteAsync(string path, object obj, Action<Exception> result)
+    public void WriteAsync(string path, object obj, Action<Exception> result)
     {
-        Task.Run(() => {
+        Task.Run(() =>
+        {
             Exception e = null;
             try
             {
@@ -115,4 +80,68 @@ public class TextSerializer
         });
     }
 
+    public class UTF8StringWriter : StringWriter
+    {
+        private static readonly Encoding UTF8 = new UTF8Encoding(false);
+        public override Encoding Encoding => UTF8;
+        public UTF8StringWriter(StringBuilder sb) : base(sb) { }
+    }
+}
+
+public class TextXmlSerializer : TextSerializer
+{
+    public override string Format => "xml";
+
+    public override string Serialize(object obj)
+    {
+        XmlSerializer xs = new XmlSerializer(obj.GetType());
+        StringBuilder sb = new StringBuilder();
+        UTF8StringWriter sw = new UTF8StringWriter(sb);
+        xs.Serialize(sw, obj);
+        sw.Close();
+        return sb.ToString();
+    }
+    public override object Deserialize(string text, Type type)
+    {
+        XmlSerializer xs = new XmlSerializer(type);
+        StringReader sr = new StringReader(text);
+        return xs.Deserialize(sr);
+    }
+}
+public class TextNewtonJsonSerializer : TextSerializer
+{
+    public override string Format => "json";
+
+    private Func<object, string> ser;
+    private Func<string, Type, object> deser;
+
+    public TextNewtonJsonSerializer()
+    {
+        var ass = Assembly.Load("Newtonsoft.Json");
+        var type = ass.GetType("Newtonsoft.Json.JsonConvert");
+
+        var serMethod = type.GetMethod("SerializeObject", new Type[] { typeof(object) });
+        this.ser = (Func<object, string>)serMethod.CreateDelegate(typeof(Func<object, string>));
+
+        var deserMethod = type.GetMethod("DeserializeObject", new Type[] { typeof(string), typeof(Type) });
+        this.deser = (Func<string, Type, object>)deserMethod.CreateDelegate(typeof(Func<string, Type, object>));
+    }
+
+    public override object Deserialize(string text, Type type)
+    {
+        if (this.deser == null)
+        {
+            throw new NotImplementedException();
+        }
+        return this.deser.Invoke(text, type);
+    }
+
+    public override string Serialize(object obj)
+    {
+        if (this.ser == null)
+        {
+            throw new NotImplementedException();
+        }
+        return this.ser.Invoke(obj);
+    }
 }
