2ff8144473156dbcaafe3f47a96263cd780c4785
JomiXedYu <JomiXedYu@outlook.com>
2021-11-01 10:32:37 +0800

    Timer

--- JxUnity/Timers/Timer.cs
+++ JxUnity/Timers/Timer.cs
@@ -72,4 +72,26 @@ namespace JxUnity.Timers
         }
 
     }
+
+    internal class TimerMono : MonoBehaviour
+    {
+        private static TimerMono instance;
+        public static TimerMono Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new GameObject($"__m_{nameof(TimerMono)}").AddComponent<TimerMono>();
+                }
+                return instance;
+            }
+        }
+
+        internal event Action OnUpdate;
+        private void Update()
+        {
+            this.OnUpdate();
+        }
+    }
 }
