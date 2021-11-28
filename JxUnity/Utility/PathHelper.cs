90bc11c309ff63eb14a2e809da661e7f45280087
JomiXedYu <JomiXedYu@outlook.com>
2021-11-28 16:13:42 +0800

    新增Mod路径

--- JxUnity/Utility/PathHelper.cs
+++ JxUnity/Utility/PathHelper.cs
@@ -14,6 +14,7 @@ public static class PathHelper
     public static readonly string Log = $"{WritablePath}/Log";
     public static readonly string ScreenShot = $"{WritablePath}/ScreenShot";
     public static readonly string Icon = $"{WritablePath}/Icon";
+    public static readonly string Mod = $"{WritablePath}/Mod";
 
 
     private static string GetWritablePath()
