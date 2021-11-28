c985bb9340fdb6b8abe34adba19eafdc3d3bd0d4
JomiXedYu <JomiXedYu@outlook.com>
2021-11-28 18:17:27 +0800

    Create Extension.cs

---
+++ JxUnity/Exentsions/Extension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JxUnity.Exentsions
+{
+    public class Extension
+    {
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        public string Version { get; protected set; }
+        public string Author { get; private set; }
+    }
+}
