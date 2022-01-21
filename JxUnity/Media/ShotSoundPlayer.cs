b2470d9bbace5ba5d8c25a937069761e54dd4ac4
JomiXedYu <JomiXedYu@outlook.com>
2022-01-22 04:04:25 +0800

    Create ShotSoundPlayer.cs

---
+++ JxUnity/Media/ShotSoundPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+namespace JxUnity.Media
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class ShotSoundPlayer : MonoBehaviour
+    {
+        private static ShotSoundPlayer instance;
+        public static ShotSoundPlayer GetInstance()
+        {
+            if (instance == null)
+            {
+                var go = new GameObject($"__m_{nameof(ShotSoundPlayer)}");
+                DontDestroyOnLoad(go);
+                instance = go.AddComponent<ShotSoundPlayer>();
+            }
+            return instance;
+        }
+
+        private AudioSource audioSource;
+        private Func<float> volumeGetter;
+
+        private void Awake()
+        {
+            this.audioSource = GetComponent<AudioSource>();
+            if (this.audioSource == null)
+            {
+                this.audioSource = this.gameObject.AddComponent<AudioSource>();
+            }
+        }
+
+
+
+        public void SetVolumeGetter(Func<float> getter)
+        {
+            this.volumeGetter = getter;
+        }
+
+        public void Play(AudioClip audioClip, float volume = 1f)
+        {
+            this.audioSource.PlayOneShot(audioClip, volume);
+        }
+
+        public void Play(AudioClip audioClip)
+        {
+            float volume = this.volumeGetter?.Invoke() ?? 1f;
+            this.audioSource.PlayOneShot(audioClip, volume);
+        }
+    }
+}
