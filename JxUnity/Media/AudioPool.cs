d64b642b669f71758777046c2877b36c6ce3b5ee
JomiXedYu <JomiXedYu@outlook.com>
2022-01-22 04:04:13 +0800

    rename

--- JxUnity/Media/AudioPool.cs
+++ JxUnity/Media/AudioPool.cs
@@ -2,47 +2,177 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class AudioPool : MonoBehaviour
+namespace JxUnity.Media
 {
-    protected class AudioState : IComparable<AudioState>
+    public class AudioPool : MonoBehaviour
     {
-        //max volume
-        private float volume;
+        protected class AudioState : IComparable<AudioState>
+        {
+            //max volume
+            private float volume;
+            public float Volume
+            {
+                get => volume;
+                set
+                {
+                    if (this.audioSource.volume > value)
+                    {
+                        this.audioSource.volume = value;
+                    }
+                    this.volume = value;
+                }
+            }
+
+            public bool IsPlaying { get => this.audioSource.isPlaying; }
+            public bool IsUsed { get; private set; }
+
+            private AudioSource audioSource;
+
+            /// <summary>
+            /// 如果name为null则为匿名音频
+            /// </summary>
+            public string Name { get; private set; }
+            private float createTime;
+            private float endTime;
+            public bool IsLoop { get; private set; }
+            private bool isFade;
+
+            private bool isToFade;
+
+            public AudioState(AudioSource audioSource)
+            {
+                this.audioSource = audioSource;
+            }
+
+            public void Play(AudioClip ac, string name = null, bool isLoop = false, bool isFade = false)
+            {
+                if (ac == null)
+                {
+                    Debug.LogError("audioclip is null");
+                    return;
+                }
+                this.isToFade = false;
+                this.Name = name;
+                this.IsLoop = isLoop;
+                this.isFade = isFade;
+                this.createTime = Time.time;
+
+                this.endTime = this.createTime + ac.length;
+
+                this.audioSource.clip = ac;
+                this.audioSource.loop = isLoop;
+                this.audioSource.volume = this.volume;
+                this.audioSource.Play();
+
+                this.IsUsed = true;
+            }
+            public void Release()
+            {
+                this.Name = null;
+                this.IsLoop = false;
+                this.audioSource.Stop();
+                this.audioSource.clip = null;
+                this.IsUsed = false;
+            }
+            public void Stop()
+            {
+                if (!this.isFade)
+                {
+                    this.Release();
+                }
+                else
+                {
+                    this.isToFade = true;
+                }
+            }
+
+            public void Update()
+            {
+                if (this.isFade && this.isToFade)
+                {
+                    var v = this.audioSource.volume;
+                    v = Math.Max(0, v - Time.unscaledDeltaTime);
+                    this.audioSource.volume = v;
+
+                    if (v <= 0)
+                    {
+                        this.Release();
+                    }
+                }
+
+                if (!this.IsPlaying)
+                {
+                    this.Release();
+                }
+            }
+
+            public int CompareTo(AudioState other)
+            {
+                var r = this.IsLoop.CompareTo(other.IsLoop);
+                if (r != 0)
+                {
+                    return r;
+                }
+                return this.endTime.CompareTo(other.endTime);
+            }
+        }
+
+        [SerializeField]
+        protected int maxCount = 5;
+        public int MaxCount { get => maxCount; }
+        protected List<AudioState> pool;
+        [SerializeField]
+        private bool[] poolState;
+
+        private float volume = 1f;
         public float Volume
         {
             get => volume;
             set
             {
-                if (this.audioSource.volume > value)
+                volume = Mathf.Clamp(value, 0, 1);
+                foreach (var item in pool)
                 {
-                    this.audioSource.volume = value;
+                    item.Volume = volume;
                 }
-                this.volume = value;
             }
         }
 
-        public bool IsPlaying { get => this.audioSource.isPlaying; }
-        public bool IsUsed { get; private set; }
-
-        private AudioSource audioSource;
-
-        /// <summary>
-        /// 如果name为null则为匿名音频
-        /// </summary>
-        public string Name { get; private set; }
-        private float createTime;
-        private float endTime;
-        public bool IsLoop { get; private set; }
-        private bool isFade;
-
-        private bool isToFade;
-
-        public AudioState(AudioSource audioSource)
+        protected void Awake()
         {
-            this.audioSource = audioSource;
+            pool = new List<AudioState>(maxCount);
+            poolState = new bool[MaxCount];
+            //创建对象
+            for (int i = 0; i < maxCount; i++)
+            {
+                AudioSource ap = gameObject.AddComponent<AudioSource>();
+                ap.volume = volume;
+                AudioState aus = new AudioState(ap);
+                pool.Add(aus);
+            }
         }
 
+        protected void Update()
+        {
+            for (int i = 0; i < this.pool.Count; i++)
+            {
+                var item = this.pool[i];
+                if (item.IsUsed)
+                {
+                    item.Update();
+                }
+            }
+            for (int i = 0; i < MaxCount; i++)
+            {
+                poolState[i] = this.pool[i].IsUsed;
+            }
+        }
+        /// <summary>
+        /// 播放一个音频
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="ac"></param>
+        /// <param name="isLoop"></param>
         public void Play(AudioClip ac, string name = null, bool isLoop = false, bool isFade = false)
         {
             if (ac == null)
@@ -50,182 +180,54 @@ public class AudioPool : MonoBehaviour
                 Debug.LogError("audioclip is null");
                 return;
             }
-            this.isToFade = false;
-            this.Name = name;
-            this.IsLoop = isLoop;
-            this.isFade = isFade;
-            this.createTime = Time.time;
 
-            this.endTime = this.createTime + ac.length;
-
-            this.audioSource.clip = ac;
-            this.audioSource.loop = isLoop;
-            this.audioSource.volume = this.volume;
-            this.audioSource.Play();
-
-            this.IsUsed = true;
-        }
-        public void Release()
-        {
-            this.Name = null;
-            this.IsLoop = false;
-            this.audioSource.Stop();
-            this.audioSource.clip = null;
-            this.IsUsed = false;
-        }
-        public void Stop()
-        {
-            if (!this.isFade)
-            {
-                this.Release();
-            }
-            else
-            {
-                this.isToFade = true;
-            }
+            AudioState aus = GetUsableAudio();
+            aus.Play(ac, name, isLoop, isFade);
         }
 
-        public void Update()
+        /// <summary>
+        /// 停止某个音频
+        /// </summary>
+        /// <param name="name"></param>
+        public void Stop(string name)
         {
-            if (this.isFade && this.isToFade)
-            {
-                var v = this.audioSource.volume;
-                v = Math.Max(0, v - Time.unscaledDeltaTime);
-                this.audioSource.volume = v;
-
-                if (v <= 0)
-                {
-                    this.Release();
-                }
-            }
-
-            if (!this.IsPlaying)
-            {
-                this.Release();
-            }
-        }
-
-        public int CompareTo(AudioState other)
-        {
-            var r = this.IsLoop.CompareTo(other.IsLoop);
-            if (r != 0)
-            {
-                return r;
-            }
-            return this.endTime.CompareTo(other.endTime);
-        }
-    }
-
-    [SerializeField]
-    protected int maxCount = 5;
-    public int MaxCount { get => maxCount; }
-    protected List<AudioState> pool;
-    [SerializeField]
-    private bool[] poolState;
-
-    private float volume = 1f;
-    public float Volume
-    {
-        get => volume;
-        set
-        {
-            volume = Mathf.Clamp(value, 0, 1);
             foreach (var item in pool)
             {
-                item.Volume = volume;
+                if (item.Name == name)
+                {
+                    item.Stop();
+                }
             }
         }
-    }
-
-    protected void Awake()
-    {
-        pool = new List<AudioState>(maxCount);
-        poolState = new bool[MaxCount];
-        //创建对象
-        for (int i = 0; i < maxCount; i++)
+        /// <summary>
+        /// 停止并释放所有音频
+        /// </summary>
+        public void StopAll()
         {
-            AudioSource ap = gameObject.AddComponent<AudioSource>();
-            ap.volume = volume;
-            AudioState aus = new AudioState(ap);
-            pool.Add(aus);
-        }
-    }
-
-    protected void Update()
-    {
-        for (int i = 0; i < this.pool.Count; i++)
-        {
-            var item = this.pool[i];
-            if (item.IsUsed)
-            {
-                item.Update();
-            }
-        }
-        for (int i = 0; i < MaxCount; i++)
-        {
-            poolState[i] = this.pool[i].IsUsed;
-        }
-    }
-    /// <summary>
-    /// 播放一个音频
-    /// </summary>
-    /// <param name="name"></param>
-    /// <param name="ac"></param>
-    /// <param name="isLoop"></param>
-    public void Play(AudioClip ac, string name = null, bool isLoop = false, bool isFade = false)
-    {
-        if (ac == null)
-        {
-            Debug.LogError("audioclip is null");
-            return;
-        }
-
-        AudioState aus = GetUsableAudio();
-        aus.Play(ac, name, isLoop, isFade);
-    }
-
-    /// <summary>
-    /// 停止某个音频
-    /// </summary>
-    /// <param name="name"></param>
-    public void Stop(string name)
-    {
-        foreach (var item in pool)
-        {
-            if (item.Name == name)
+            foreach (var item in pool)
             {
                 item.Stop();
             }
         }
-    }
-    /// <summary>
-    /// 停止并释放所有音频
-    /// </summary>
-    public void StopAll()
-    {
-        foreach (var item in pool)
-        {
-            item.Stop();
-        }
-    }
 
-    public void Clear()
-    {
-        foreach (var item in this.pool)
+        public void Clear()
         {
-            item.Release();
-        }
-    }
-
-    protected AudioState GetUsableAudio()
-    {
-        //先找空闲的，没有空闲的按时间排，查找非循环和最早创建的
-        foreach (var item in pool)
-        {
-            if (!item.IsUsed) return item;
+            foreach (var item in this.pool)
+            {
+                item.Release();
+            }
         }
 
-        pool.Sort((x, y) => x.CompareTo(y));
-        return pool[0];
+        protected AudioState GetUsableAudio()
+        {
+            //先找空闲的，没有空闲的按时间排，查找非循环和最早创建的
+            foreach (var item in pool)
+            {
+                if (!item.IsUsed) return item;
+            }
+
+            pool.Sort((x, y) => x.CompareTo(y));
+            return pool[0];
+        }
     }
 }
--- JxUnity/Media/MusicPlayer.cs
+++ JxUnity/Media/MusicPlayer.cs
@@ -2,200 +2,203 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
-public class MusicPlayer : MonoBehaviour
+namespace JxUnity.Media
 {
-    private AudioSource audioSource;
-    private AudioClip clip;
-
-    public AudioClip Clip { get => clip; }
-
-    [SerializeField]
-    private bool isLoop = false;
-    public bool IsLoop { get => isLoop; set => isLoop = value; }
-
-    [SerializeField]
-    private bool isPlaying = false;
-    public bool IsPlaying { get => isPlaying; }
-
-    public event Action OnEndHandler;
-
-    [SerializeField]
-    private float transision = 1f;
-    public float Transision { get => transision; }
-
-    //最大音量 非实际音量
-    private float volume = 1f;
-    public float Volume
+    [RequireComponent(typeof(UnityEngine.AudioSource))]
+    public class MusicPlayer : MonoBehaviour
     {
-        get => volume;
-        set
+        private AudioSource audioSource;
+        private AudioClip clip;
+
+        public AudioClip Clip { get => clip; }
+
+        [SerializeField]
+        private bool isLoop = false;
+        public bool IsLoop { get => isLoop; set => isLoop = value; }
+
+        [SerializeField]
+        private bool isPlaying = false;
+        public bool IsPlaying { get => isPlaying; }
+
+        public event Action OnEndHandler;
+
+        [SerializeField]
+        private float transision = 1f;
+        public float Transision { get => transision; }
+
+        //最大音量 非实际音量
+        private float volume = 1f;
+        public float Volume
         {
-            value = Math.Max(Math.Min(value, 1), 0);
-            if (!isToMax && !isToMin)
+            get => volume;
+            set
             {
-                audioSource.volume = value;
+                value = Math.Max(Math.Min(value, 1), 0);
+                if (!isToMax && !isToMin)
+                {
+                    audioSource.volume = value;
+                }
+                volume = value;
             }
-            volume = value;
         }
-    }
 
-    private Action transAct;
-    private bool isToMax = false;
-    private bool isToMin = false;
+        private Action transAct;
+        private bool isToMax = false;
+        private bool isToMin = false;
 
-    [SerializeField]
-    private float playedTime = 0;
-    [SerializeField]
-    private float clipTime = 0;
+        [SerializeField]
+        private float playedTime = 0;
+        [SerializeField]
+        private float clipTime = 0;
 
-    private static MusicPlayer instance;
-    public static MusicPlayer GetInstance()
-    {
-        if (instance == null)
+        private static MusicPlayer instance;
+        public static MusicPlayer GetInstance()
         {
-            instance = GameObject.FindObjectOfType<MusicPlayer>();
             if (instance == null)
             {
-                instance = new GameObject(nameof(MusicPlayer)).AddComponent<MusicPlayer>();
+                instance = GameObject.FindObjectOfType<MusicPlayer>();
+                if (instance == null)
+                {
+                    instance = new GameObject(nameof(MusicPlayer)).AddComponent<MusicPlayer>();
+                }
+            }
+            return instance;
+        }
+
+        private void Awake()
+        {
+            instance = this;
+            audioSource = GetComponent<AudioSource>();
+            audioSource.loop = false;
+            audioSource.playOnAwake = false;
+
+            if (audioSource.playOnAwake)
+            {
+                this.Play();
             }
         }
-        return instance;
-    }
-    
-    private void Awake()
-    {
-        instance = this;
-        audioSource = GetComponent<AudioSource>();
-        audioSource.loop = false;
-        audioSource.playOnAwake = false;
 
-        if (audioSource.playOnAwake)
+        private void Update()
         {
-            this.Play();
-        }
-    }
-
-    private void Update()
-    {
-        if (isToMax)
-        {
-            if (audioSource.volume < volume)
+            if (isToMax)
             {
-                audioSource.volume += Time.deltaTime * 1 / transision;
+                if (audioSource.volume < volume)
+                {
+                    audioSource.volume += Time.deltaTime * 1 / transision;
+                }
+                else
+                {
+                    audioSource.volume = volume;
+                    isToMax = false;
+                    OnToMax();
+                }
+            }
+            else if (isToMin)
+            {
+                if (audioSource.volume > 0)
+                {
+                    audioSource.volume -= Time.deltaTime * 1 / transision;
+                }
+                else
+                {
+                    audioSource.volume = 0;
+                    isToMin = false;
+                    OnToMin();
+                }
+            }
+
+            if (this.isPlaying && Time.frameCount % 60 == 0)
+            {
+                playedTime = audioSource.time;
+                if (audioSource.time == 0)
+                    OnEnd();
+            }
+        }
+
+        private void ToMin()
+        {
+            isToMin = true;
+            isToMax = false;
+        }
+        private void ToMax()
+        {
+            isToMin = false;
+            isToMax = true;
+        }
+        private void OnToMin()
+        {
+            if (transAct != null)
+            {
+                transAct.Invoke();
+                transAct = null;
+            }
+        }
+        private void OnToMax()
+        {
+
+        }
+
+
+        private void OnEnd()
+        {
+            OnEndHandler?.Invoke();
+            if (isLoop)
+            {
+                audioSource.time = 0;
+                this.Play();
             }
             else
             {
-                audioSource.volume = volume;
-                isToMax = false;
-                OnToMax();
+                isPlaying = false;
             }
+
         }
-        else if (isToMin)
+
+        public void Play()
         {
-            if (audioSource.volume > 0)
-            {
-                audioSource.volume -= Time.deltaTime * 1 / transision;
-            }
-            else
-            {
-                audioSource.volume = 0;
-                isToMin = false;
-                OnToMin();
-            }
+            isPlaying = true;
+            audioSource.Play();
+            ToMax();
         }
-
-        if (this.isPlaying && Time.frameCount % 60 == 0)
-        {
-            playedTime = audioSource.time;
-            if (audioSource.time == 0)
-                OnEnd();
-        }
-    }
-
-    private void ToMin()
-    {
-        isToMin = true;
-        isToMax = false;
-    }
-    private void ToMax()
-    {
-        isToMin = false;
-        isToMax = true;
-    }
-    private void OnToMin()
-    {
-        if (transAct != null)
-        {
-            transAct.Invoke();
-            transAct = null;
-        }
-    }
-    private void OnToMax()
-    {
-
-    }
-
-
-    private void OnEnd()
-    {
-        OnEndHandler?.Invoke();
-        if (isLoop)
-        {
-            audioSource.time = 0;
-            this.Play();
-        }
-        else
+        public void Pause()
         {
             isPlaying = false;
+
+            transAct = audioSource.Pause;
+            ToMin();
         }
 
-    }
-
-    public void Play()
-    {
-        isPlaying = true;
-        audioSource.Play();
-        ToMax();
-    }
-    public void Pause()
-    {
-        isPlaying = false;
-
-        transAct = audioSource.Pause;
-        ToMin();
-    }
-
-    public void Play(AudioClip audioClip)
-    {
-        clip = audioClip;
-        if (isPlaying)
+        public void Play(AudioClip audioClip)
         {
-            transAct = () =>
+            clip = audioClip;
+            if (isPlaying)
             {
+                transAct = () =>
+                {
+                    clipTime = clip.length;
+                    audioSource.clip = clip;
+                    audioSource.Play();
+                    ToMax();
+                };
+                ToMin();
+            }
+            else
+            {
+
+                isPlaying = true;
                 clipTime = clip.length;
                 audioSource.clip = clip;
                 audioSource.Play();
                 ToMax();
-            };
+            }
+        }
+
+        public void Stop()
+        {
+            isPlaying = false;
+
+            transAct = audioSource.Stop;
             ToMin();
         }
-        else
-        {
-
-            isPlaying = true;
-            clipTime = clip.length;
-            audioSource.clip = clip;
-            audioSource.Play();
-            ToMax();
-        }
-    }
-
-    public void Stop()
-    {
-        isPlaying = false;
-
-        transAct = audioSource.Stop;
-        ToMin();
     }
 }
