b61cfb933138b385d3d9ede14c065bedb7aeaf29
JomiXedYu <JomiXedYu@outlook.com>
2022-01-23 20:39:07 +0800

    Update StatusLabel.cs
    新增文本，电量显示。
    新增坐标位移
    新增显示位置

--- JxUnity/Utility/StatusLabel.cs
+++ JxUnity/Utility/StatusLabel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 namespace JxUnity.Utility
@@ -10,11 +11,66 @@ namespace JxUnity.Utility
         string OnRenderText();
     }
 
+    /// <summary>
+    /// 每秒网络延迟标签
+    /// </summary>
+    public class StatusLabelNetDelay : IStatusLabel
+    {
+        private Func<float> getter;
+        public StatusLabelNetDelay(Func<float> getter)
+        {
+            this.getter = getter;
+        }
+        public string OnRenderText()
+        {
+            return this.getter.Invoke() + "ms";
+        }
+
+        public void OnUpdate(float udt)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// 纯文本
+    /// </summary>
+    public class StatusLabelText : IStatusLabel
+    {
+        private Func<string> getter;
+        private string text;
+        /// <summary>
+        /// 动态文本
+        /// </summary>
+        /// <param name="getter"></param>
+        public StatusLabelText(Func<string> getter)
+        {
+            this.getter = getter;
+        }
+
+        /// <summary>
+        /// 静态文本
+        /// </summary>
+        /// <param name="text"></param>
+        public StatusLabelText(string text)
+        {
+            this.text = text;
+        }
+        public string OnRenderText()
+        {
+            return this.getter != null ? this.getter.Invoke() : this.text;
+        }
+
+        public void OnUpdate(float udt) { }
+    }
+    /// <summary>
+    /// 每秒帧率标签
+    /// </summary>
     public class StatusLabelFps : IStatusLabel
     {
         private float time;
         private int frameCount;
-        private string showText;
+        private string showText = "fps: 0";
 
         public void OnUpdate(float udt)
         {
@@ -34,12 +90,30 @@ namespace JxUnity.Utility
             return this.showText;
         }
     }
-
-
-    internal class StatusLabel : MonoBehaviour
+    public class StatusLabelBattery : IStatusLabel
     {
+        public string OnRenderText()
+        {
+            return "battery: "+ (SystemInfo.batteryLevel * 100).ToString() + "%";
+        }
+
+        public void OnUpdate(float udt)
+        {
+        }
+    }
+
+    public class StatusLabel : MonoBehaviour
+    {
+        public enum PositionType
+        {
+            UpperLeft,
+            UpperRight,
+            LowerLeft,
+            LowerRight,
+        }
+
         private static StatusLabel instance;
-        private static StatusLabel Instance
+        public static StatusLabel Instance
         {
             get
             {
@@ -53,11 +127,54 @@ namespace JxUnity.Utility
             }
         }
 
+        public Vector2Int offset { get; set; }
+        public static Vector2Int Offset { get => Instance.offset; set => Instance.offset = value; }
+
         private IList<IStatusLabel> status;
+        private StringBuilder sb;
+        private string text;
+
+        private GUIStyle style;
+
+        private PositionType _positionType;
+        public PositionType positionType
+        {
+            get => this._positionType;
+            set
+            {
+                this._positionType = value;
+                switch (value)
+                {
+                    case PositionType.UpperLeft:
+                        this.style.alignment = TextAnchor.UpperLeft;
+                        break;
+                    case PositionType.UpperRight:
+                        this.style.alignment = TextAnchor.UpperRight;
+                        break;
+                    case PositionType.LowerLeft:
+                        this.style.alignment = TextAnchor.LowerLeft;
+                        break;
+                    case PositionType.LowerRight:
+                        this.style.alignment = TextAnchor.LowerRight;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static PositionType Position { get => Instance.positionType; set => Instance.positionType = value; }
 
         private void Awake()
         {
             this.status = new List<IStatusLabel>();
+            this.sb = new StringBuilder();
+            this.text = string.Empty;
+
+            this.style = new GUIStyle();
+            this.style.normal.textColor = Color.white;
+
+            this.positionType = PositionType.UpperLeft;
         }
 
         public static void AddLabel(IStatusLabel statusLabel)
@@ -69,25 +186,40 @@ namespace JxUnity.Utility
             }
             self.status.Add(statusLabel);
         }
+
+        float dt = 0;
         private void Update()
         {
+            float udt = Time.unscaledDeltaTime;
             foreach (var item in this.status)
             {
-                item.OnUpdate(Time.unscaledDeltaTime);
+                item.OnUpdate(udt);
             }
+            this.dt += udt;
         }
+
         private void OnGUI()
         {
-            foreach (var item in this.status)
+            if (this.dt > 1f)
             {
-                string txt = item.OnRenderText();
-                if (txt == null)
+                this.dt = 0f;
+                int count = this.status.Count;
+                for (int i = 0; i < count; i++)
                 {
-                    continue;
+                    var item = this.status[i];
+                    sb.Append(item.OnRenderText());
+                    if (i != count - 1) //no last
+                    {
+                        sb.Append("    ");
+                    }
                 }
-                float width = txt.Length * 8;
-                GUI.Label(new Rect(0, 0, width, 20), txt);
+                this.text = sb.ToString();
+                sb.Clear();
             }
+
+            int width = Screen.width - offset.x * 2;
+            int height = Screen.height - offset.y * 2;
+            GUI.Label(new Rect(offset.x, offset.y, width, height), this.text, style);
 
         }
     }
