eab414c301a129c0fe1ae8d6572822179623bfa3
JomiXedYu <JomiXedYu@outlook.com>
2022-02-06 16:23:34 +0800

    Update JuiInspector.cs

--- JxUnity/Jxugui/Editor/JuiInspector.cs
+++ JxUnity/Jxugui/Editor/JuiInspector.cs
@@ -23,13 +23,65 @@ public class JuiInspector : EditorWindow
         }
         return ui.Name + "  { class " + ui.GetType().Name + " }";
     }
+
+    private Dictionary<int, string> levelTabCache = new Dictionary<int, string>()
+    {
+        {0, "" },
+        {1, "\t" },
+        {2, "\t\t" },
+        {3, "\t\t\t" },
+        {4, "\t\t\t\t" },
+        {5, "\t\t\t\t\t" },
+        {6, "\t\t\t\t\t\t" },
+        {7, "\t\t\t\t\t\t\t" },
+        {8, "\t\t\t\t\t\t\t\t" },
+    };
+
+    private string GetLevelTab(int level)
+    {
+        string value;
+        if (!levelTabCache.TryGetValue(level, out value))
+        {
+            for (int i = 0; i < level; i++)
+            {
+                value += "\t";
+            }
+        }
+        return value;
+    }
+
+    private void ShowSubUI(JuiAbstract pui, int level = 0)
+    {
+        if (pui.GetSubUICount() == 0)
+        {
+            return;
+        }
+
+        foreach (JuiSubBase subui in pui.GetSubUIs())
+        {
+            EditorGUILayout.LabelField(GetLevelTab(level + 2) + "- " + GetName(subui));
+            EditorGUILayout.LabelField(GetLevelTab(level + 3) + "is show: " + subui.IsShow.ToString());
+            var _sub = subui.GetSubUIFocus();
+            if (_sub != null)
+            {
+                EditorGUILayout.LabelField(GetLevelTab(level + 3) + "subui focus: " + _sub.Name);
+                ShowSubUI(subui, level + 1);
+            }
+        }
+    }
+
+    private Vector2 scrollView;
+
     private void OnGUI()
     {
+
         if (!JuiManager.HasInstance)
         {
             EditorGUILayout.LabelField("No Instance");
             return;
         }
+
+        this.scrollView = EditorGUILayout.BeginScrollView(this.scrollView);
         EditorGUILayout.LabelField("Focus ui: " + JuiManager.Instance.GetFocus()?.Name);
         foreach (var ui in JuiManager.Instance.GetAllUI())
         {
@@ -43,13 +95,14 @@ public class JuiInspector : EditorWindow
                 EditorGUILayout.LabelField("\tis show: " + pui.IsShow.ToString());
                 EditorGUILayout.LabelField("\tsubui focus: " + GetName(pui.GetSubUIFocus()));
                 EditorGUILayout.LabelField("\tsubui: ");
+                this.ShowSubUI(pui);
                 foreach (JuiSubBase subui in pui.GetSubUIs())
                 {
                     EditorGUILayout.LabelField("\t\t- " + GetName(subui));
                     EditorGUILayout.LabelField("\t\t\tis show: " + subui.IsShow.ToString());
                 }
             }
-
         }
+        EditorGUILayout.EndScrollView();
     }
 }
