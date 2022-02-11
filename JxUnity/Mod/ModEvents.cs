af95aad3299797979699d2b2fdd5361d7241ba4c
JomiXedYu <JomiXedYu@outlook.com>
2022-02-12 03:21:58 +0800

    Update ModEvents.cs

--- JxUnity/Mod/ModEvents.cs
+++ JxUnity/Mod/ModEvents.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace JxUnity.Exentsions
 {
-    public class ModEventArgs
+    public class ModEventArgs : IDisposable
     {
+        public virtual void Dispose()
+        {
+        }
     }
 
-    public delegate void ModEvent(object sender, ModEventArgs _e);
+    public delegate void ModEventHandler(object sender, ModEventArgs _e);
 
     //鉴于Mod功能叠加与修改的复杂性，Mod应有严谨的启动顺序，应不支持在游戏过程中对Mod的加载与卸载，仅在启动时对Mod进行顺序加载。
     public class ModInfo
@@ -22,12 +26,12 @@ namespace JxUnity.Exentsions
 
     internal class ModEventTrigger
     {
-        private List<ModEvent> Receiver = new List<ModEvent>();
-        public void Add(ModEvent receiver)
+        private List<ModEventHandler> Receiver = new List<ModEventHandler>();
+        public void Add(ModEventHandler receiver)
         {
             this.Receiver.Add(receiver);
         }
-        public void Remove(ModEvent receiver)
+        public void Remove(ModEventHandler receiver)
         {
             this.Receiver.Remove(receiver);
         }
@@ -44,8 +48,7 @@ namespace JxUnity.Exentsions
     {
         private static Dictionary<string, ModEventTrigger> Channels
             = new Dictionary<string, ModEventTrigger>();
-
-        public static void Subscribe(string eventId, ModEvent receiver)
+        public static void Subscribe(string eventId, ModEventHandler receiver)
         {
             ModEventTrigger trigger;
             if (!Channels.TryGetValue(eventId, out trigger))
@@ -55,7 +58,7 @@ namespace JxUnity.Exentsions
             }
             trigger.Add(receiver);
         }
-        public static void Unsubscribe(string eventId, ModEvent receiver)
+        public static void Unsubscribe(string eventId, ModEventHandler receiver)
         {
             ModEventTrigger trigger;
             if (!Channels.TryGetValue(eventId, out trigger))
@@ -74,26 +77,78 @@ namespace JxUnity.Exentsions
         }
     }
 
+    public static class ModLoader
+    {
+        public static void LoadMod(Type type)
+        {
+            var inst = (ModBase)Activator.CreateInstance(type);
+
+        }
+        public static void Load(Assembly assembly)
+        {
+            foreach (var item in assembly.GetTypes())
+            {
+                if (item.IsDefined(typeof(ModAttribute), false))
+                {
+                    LoadMod(item);
+                }
+            }
+        }
+    }
+
     public abstract class ModBase
     {
         public ModInfo Info { get; protected set; }
-        public virtual void OnIntialize() { }
-        public virtual void OnTerminate() { }
+        public ModBase()
+        {
+            foreach (var item in this.GetType().GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+            {
+                if (!item.IsDefined(typeof(ModEventHandlerAttribute)))
+                {
+                    continue;
+                }
+                var attr = item.GetCustomAttribute<ModEventHandlerAttribute>();
+                string eventId = attr.EventId ?? item.Name;
+                var delg = (ModEventHandler)item.CreateDelegate(typeof(ModEventHandler), this);
+                ModEventBus.Subscribe(eventId, delg);
+            }
+        }
+
     }
+
+
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public sealed class ModAttribute : Attribute
+    {
+
+    }
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public sealed class ModEventHandlerAttribute : Attribute
+    {
+        public string EventId { get; set; }
+        public ModEventHandlerAttribute()
+        {
+        }
+
+        public ModEventHandlerAttribute(string eventId)
+        {
+            this.EventId = eventId;
+        }
+    }
+
 
     public class OnGameSaveArgs : ModEventArgs
     {
         public bool isBreak;
     }
 
-    public class ExampleExtension : ModBase
+    [Mod]
+    public class ExampleMod : ModBase
     {
-        public override void OnIntialize()
-        {
-            ModEventBus.Subscribe("on_game_save", OnTake);
-        }
 
-        private void OnTake(object sender, ModEventArgs args)
+        [ModEventHandler]
+        private void OnGameSaved(object sender, ModEventArgs args)
         {
             var save = args as OnGameSaveArgs;
             ModEventBus.Broadcast("asd", null, null);
