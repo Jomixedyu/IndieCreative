50a0f87c48e065e243dfae5152eaf5830cdaf0a0
JomiXedYu <JomiXedYu@outlook.com>
2022-02-12 17:58:10 +0800

    Create MathExtension.cs

---
+++ JxUnity/Utility/MathExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+
+public static class MathExtension
+{
+    public static Color Saturate(this Color self)
+    {
+        return new Color(Mathf.Clamp01(self.r), Mathf.Clamp01(self.g), Mathf.Clamp01(self.b), Mathf.Clamp01(self.a));
+    }
+    public static Color Add(this Color self, float scalar)
+    {
+        return new Color(self.r + scalar, self.g + scalar, self.b + scalar, self.a + scalar);
+    }
+}
+
