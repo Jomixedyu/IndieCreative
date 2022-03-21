3beab03ccc077b40162b87dc9f958969889d5b0a
JomiXedYu <JomiXedYu@outlook.com>
2022-03-21 19:18:14 +0800

    Create PlatformBase.cs

---
+++ JxUnity/Platforms/PlatformBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JxUnity.Platforms
+{
+    public interface IPlatform
+    {
+        string GetFolderPath(string type);
+    }
+
+    public class LocalPlatform : IPlatform
+    {
+        public string GetFolderPath(string type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class SteamPlatform : IPlatform
+    {
+        public string GetFolderPath(string type)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
